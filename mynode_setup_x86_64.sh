4cc960ff2048ea258a79afe34d70d0a6d09e3245
Albercoin <68326029+albercoin@users.noreply.github.com>
2023-10-22 10:26:29 +0200

    Update mynode_setup_x86_64.sh

--- mynode_setup_x86_64.sh
+++ mynode_setup_x86_64.sh
@@ -328,8 +328,8 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 # Add the repository to Apt sources:
 sudo bash -c "echo \
-    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$LINUX \
-    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+    'deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$LINUX \
+    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable' | \
     tee /etc/apt/sources.list.d/docker.list > /dev/null"
 sudo apt-get update
 
